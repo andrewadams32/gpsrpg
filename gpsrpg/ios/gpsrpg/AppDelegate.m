a39814fc3b8395f62f4b5ce901b47fe85eef6a50
Andrew Adams <andydrew313@gmail.com>
2019-09-05 19:05:14 -0700

    remove api keys and initial commit

--- gpsrpg/ios/gpsrpg/AppDelegate.m
+++ gpsrpg/ios/gpsrpg/AppDelegate.m
@@ -18,7 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyDGcSTah094p6Sedesh0UZZ1i3DbA8IY78"];
+  [GMSServices provideAPIKey:@"api key goes here"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"gpsrpg"
